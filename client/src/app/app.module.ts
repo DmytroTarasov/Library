d53e5d74736fb5ddbfa2f0529f9a3da3bfee4475
DmytroTarasov <dtarasov24s@gmail.com>
2023-02-21 10:46:21 +0200

    add spinner

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,38 +1,40 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faEdit, faEye } from '@fortawesome/free-regular-svg-icons';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BookListComponent } from './books/book-list/book-list.component';
 import { BookListItemComponent } from './books/book-list-item/book-list-item.component';
 
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { TruncatePipe } from './_pipes/truncate.pipe';
-import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ModalModule } from 'ngx-bootstrap/modal';
-
-import { faEdit, faEye } from '@fortawesome/free-regular-svg-icons';
 import { ViewBookComponent } from './books/view-book/view-book.component';
-import { LengthPipe } from './_pipes/length.pipe';
 import { EditBookComponent } from './books/edit-book/edit-book.component';
 import { TextInputComponent } from './shared/_forms/text-input/text-input.component';
 import { TextareaComponent } from './shared/_forms/textarea/textarea.component';
 import { FileUploadComponent } from './shared/file-upload/file-upload.component';
+import { LengthPipe } from './_pipes/length.pipe';
+import { TruncatePipe } from './_pipes/truncate.pipe';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookListComponent,
     BookListItemComponent,
-    TruncatePipe,
     ViewBookComponent,
-    LengthPipe,
     EditBookComponent,
     TextInputComponent,
     TextareaComponent,
-    FileUploadComponent
+    FileUploadComponent,
+    LengthPipe,
+    TruncatePipe
   ],
   imports: [
     BrowserModule,
@@ -42,10 +44,14 @@ import { FileUploadComponent } from './shared/file-upload/file-upload.component'
     HttpClientModule,
     TabsModule.forRoot(),
     ModalModule.forRoot(),
-    FontAwesomeModule
+    FontAwesomeModule,
+    NgxSpinnerModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {
   constructor(library: FaIconLibrary) {
